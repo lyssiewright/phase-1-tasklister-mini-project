e587d0cb63efeb47bb8a6287f01740447eb0cd48
Lyssie <lyssiewright.15@gmail.com>
2022-02-07 19:35:42 -0600

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildTask(e.target.new_task_description.value)
+    form.reset()
+
+  })
+})
+
+function buildTask(task){
+  let ul = document.createElement('ul')
+  let bttn = document.createElement('button')
+  bttn.addEventListener('click', handleDelete)
+  ul.textContent = `${task} `
+  bttn.textContent = 'x'
+  ul.appendChild(bttn)
+  document.querySelector('ul').appendChild(ul)
+}
+
+function  handleDelete(e){
+  (e.target.parentNode.remove())
+}
